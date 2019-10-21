23e0c3e8d5f94d47d30f4522dd0f35bcf31db83d
Esta Prangel <esta@starcloud.ee>
2019-10-21 17:03:31 +0300

    Fixes, but still WIP

--- main.py
+++ main.py
@@ -6,12 +6,28 @@ from config import api_key
 
 
 apikey = api_key
-artist = 'pearl jam'
-album = 'ten'
-url = 'http://ws.audioscrobbler.com/2.0/?method=album.gettoptags&artist=' +  artist + '&album=' + album + '&api_key=' + apikey  + '&format=json'
 
-r = requests.get(url)
-r = r.text
-album_tags = json.loads(r)
+def get_top_tag(artist, album):
+    url = 'http://ws.audioscrobbler.com/2.0/?method=album.gettoptags&artist=' +  artist + '&album=' + album + '&api_key=' + apikey  + '&format=json'
 
-print(album_tags[0][0][1])
+    r = requests.get(url)
+    r = r.text
+    album_tags = json.loads(r)
+
+    return album_tags['toptags']['tag'][1]['name']
+
+
+with open("1001.txt", "r", encoding="utf-8") as reader:
+    line = reader.readline()
+    artist_pattern = re.compile(r'.*(?=\s\-)')
+    album_pattern = re.compile(r'(?<=-\s).*')
+
+    while line != '':
+        if artist_pattern.search(line) != None:
+            result = artist_pattern.search(line)
+            artist = result.group(0)
+        if album_pattern.search(line) != None:
+            result = album_pattern.search(line)
+            album = result.group(0)
+        print('artist: ' + artist + ' album: ' + album + get_top_tag(artist, album)
+        line = reader.readline()
