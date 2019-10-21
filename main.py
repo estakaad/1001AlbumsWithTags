b369d62205c1f94a02db3b1770abc6813bff3cd7
Esta Prangel <esta.prangel@gmail.com>
2019-10-21 22:52:16 +0300

    Handling exceptions. Writes artists, albums and tag names to a new file.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import requests
 import re
 import json
@@ -7,9 +6,9 @@ from config import api_key
 
 apikey = api_key
 
+
 def get_top_tag(artist, album):
     url = 'http://ws.audioscrobbler.com/2.0/?method=album.gettoptags&artist=' + artist + '&album=' + album + '&api_key=' + apikey + '&format=json'
-
     r = requests.get(url)
     r = r.text
     album_tags = json.loads(r)
@@ -17,26 +16,41 @@ def get_top_tag(artist, album):
     tag_count = 5
     tags = []
 
-    if len(album_tags['toptags']['tag']) < 5:
-        tag_count = len(album_tags['toptags']['tag'])
-
-    for item in album_tags['toptags']['tag'][:tag_count]:
-        tags.append(item['name'])
+    if 'toptags' in album_tags:
+        if len(album_tags['toptags']['tag']) < 5:
+            tag_count = len(album_tags['toptags']['tag'])
+        if album_tags['toptags']['tag']:
+            for item in album_tags['toptags']['tag'][:tag_count]:
+                tags.append(item['name'])
+        else:
+            tags.append('N/A')
+    else:
+        tags.append('N/A')
 
     return tags
 
 
-with open("1001.txt", "r", encoding="utf-8") as reader:
-    line = reader.readline()
-    artist_pattern = re.compile(r'.*(?=\s\-)')
-    album_pattern = re.compile(r'(?<=-\s).*')
-
-    while line != '':
-        if artist_pattern.search(line) != None:
-            result = artist_pattern.search(line)
-            artist = result.group(0)
-        if album_pattern.search(line) != None:
-            result = album_pattern.search(line)
-            album = result.group(0)
-        print('artist: ' + artist + ' album: ' + album + get_top_tag(artist, album))
+def write_results_to_file(input_file):
+    with open(input_file, 'r', encoding='utf-8') as reader:
         line = reader.readline()
+        artist_pattern = re.compile(r'.*(?=\s\-)')
+        the_artist_pattern = re.compile(r'.*(?=,\sThe\s)')
+        album_pattern = re.compile(r'(?<=-\s).*')
+
+        while line != '':
+            with open('albums_with_tags.txt', 'a', encoding='utf-8') as results_file:
+                if artist_pattern.search(line) != None:
+                    if the_artist_pattern.search(line) != None:
+                        result = the_artist_pattern.search(line)
+                        artist = 'The ' + result.group(0)
+                    else:
+                        result = artist_pattern.search(line)
+                        artist = result.group(0)
+                if album_pattern.search(line) != None:
+                    result = album_pattern.search(line)
+                    album = result.group(0)
+                tags = get_top_tag(artist, album)
+                results_file.write(artist + ' - ' + album + ' - ' + ', '.join(tags) + '\n')
+                line = reader.readline()
+
+write_results_to_file('1001.txt')
