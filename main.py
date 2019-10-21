969cfbb714c27e75ff4b4807396e957a19d070b8
Esta Prangel <esta.prangel@gmail.com>
2019-10-21 20:59:42 +0300

    Get five top tags for album

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ from config import api_key
 apikey = api_key
 
 def get_top_tag(artist, album):
-    url = 'http://ws.audioscrobbler.com/2.0/?method=album.gettoptags&artist=' +  artist + '&album=' + album + '&api_key=' + apikey  + '&format=json'
+    url = 'http://ws.audioscrobbler.com/2.0/?method=album.gettoptags&artist=' + artist + '&album=' + album + '&api_key=' + apikey + '&format=json'
 
     r = requests.get(url)
     r = r.text
     album_tags = json.loads(r)
 
-    return album_tags['toptags']['tag'][1]['name']
+    tag_count = 5
+    tags = []
+
+    if len(album_tags['toptags']['tag']) < 5:
+        tag_count = len(album_tags['toptags']['tag'])
+
+    for item in album_tags['toptags']['tag'][:tag_count]:
+        tags.append(item['name'])
+
+    return tags
 
 
 with open("1001.txt", "r", encoding="utf-8") as reader:
@@ -29,5 +38,5 @@ with open("1001.txt", "r", encoding="utf-8") as reader:
         if album_pattern.search(line) != None:
             result = album_pattern.search(line)
             album = result.group(0)
-        print('artist: ' + artist + ' album: ' + album + get_top_tag(artist, album)
+        print('artist: ' + artist + ' album: ' + album + get_top_tag(artist, album))
         line = reader.readline()
